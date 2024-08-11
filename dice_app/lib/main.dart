71430323d08d2ce79caca9131e6b88168bce4a1d
tahaasif04 <114299535+tahaasif04@users.noreply.github.com>
2024-08-11 16:06:43 +0500

    Update main.dart

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -2,11 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 78, 40, 182),
-        body: Center(
-          child: Text('Hello World'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 60, 12, 105),
+                Color.fromARGB(255, 29, 56, 175)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text('Hello World',style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+            ),),
+          ),
         ),
       ),
     ),
