e7118c207ff84a63dd1f83d81bfd82491e2c1208
tahaasif04 <114299535+tahaasif04@users.noreply.github.com>
2024-09-27 21:55:20 +0500

    dff

--- dice_app/lib/gradient_container.dart
+++ dice_app/lib/gradient_container.dart
@@ -1,45 +1,45 @@
 import 'package:flutter/material.dart';
 
-
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
 
-class GradientContainer extends StatelessWidget
-{
-  const GradientContainer(this.color1,this.color2,{super.key});
+  final Color color1;
+  final Color color2;
 
-final Color color1;
-final Color color2;
-
-void rollDice(){
-  
-}
+  void rollDice() {}
 
   @override
-  Widget build(context){
-  return  Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [color1,color2],
-              begin: startAlignment ,
-              end: endAlignment ,
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-1.png',
+              width: 200,
             ),
-          ),
-          child: Center(
-            child: Column(
-              children: [
-                Image.asset(
-                  'assets/images/dice-1.png',width:200,
-                  ),
-          
-          TextButton(
-            onPressed: rollDice,
-            child: const Text('Roll Dice'),
-
-          )
-          ], 
+           const SizedBox(height: 20),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 28)),
+              child: const Text('Roll Dice'),
+            )
+          ],
         ),
-        ),
-  );
-        }}
+      ),
+    );
+  }
+}
