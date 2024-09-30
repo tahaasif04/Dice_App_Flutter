287a2fc5846ad89eadcde10de81c6eb85759aad7
tahaasif04 <114299535+tahaasif04@users.noreply.github.com>
2024-09-30 09:45:56 +0500

    Update gradient_container.dart

--- dice_app/lib/gradient_container.dart
+++ dice_app/lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:dice_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget
 {
   const GradientContainer({super.key});
@@ -13,8 +16,8 @@ class GradientContainer extends StatelessWidget
                 Color.fromARGB(255, 60, 12, 105),
                 Color.fromARGB(255, 29, 56, 175)
               ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
+              begin: startAlignment,
+              end: endAlignment,
             ),
           ),
           child: const Center(
