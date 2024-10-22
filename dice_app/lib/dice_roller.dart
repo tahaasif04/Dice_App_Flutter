eb3942ed1893f3d77f9f8e5427d30f232753ed30
tahaasif04 <114299535+tahaasif04@users.noreply.github.com>
2024-10-22 09:25:27 +0500

    Update dice_roller.dart

--- dice_app/lib/dice_roller.dart
+++ dice_app/lib/dice_roller.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 
 final randomizer = Random();
 
-class DiceRoller extends StatefulWidget{
+class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
   @override
   State<DiceRoller> createState() {
@@ -11,34 +11,33 @@ class DiceRoller extends StatefulWidget{
   }
 }
 
-class _DiceRollerState extends State<DiceRoller>{
+class _DiceRollerState extends State<DiceRoller> {
   var currentDiceRoll = 2;
-  
-  void rollDice(){
+
+  void rollDice() {
     setState(() {
-      currentDiceRoll = randomizer.nextInt(6)+1;
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
-  @override
-  Widget build(context){
-    return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-                 Image.asset(
-                  'assets/images/dice-$currentDiceRoll.png', 
-                  width: 200,
-                 ),
-                 const SizedBox(height: 20),
-      TextButton(
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(fontSize:28)
-            ),
-            child: const Text('Roll Dice'),
 
-          ),
-          ], 
-         );
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 200,
+        ),
+        const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 28)),
+          child: const Text('Roll Dice'),
+        ),
+      ],
+    );
   }
 }
